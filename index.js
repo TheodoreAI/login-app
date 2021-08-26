13ad819e5146b1c33383f7623e9ffafda99c9ae5
Mateo Estrada <mateoej12@gmail.com>
2021-08-25 17:52:18 -0700

    Added new funtionalities.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const path = require('path');
-
+require('dotenv').config();
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -12,8 +12,35 @@ app.use('/static', express.static('public'));
 
 // @TODO add auth middleware
 // @TODO add registration page
+app.use(
+    require('express-session')({
+      secret: process.env.APP_SECRET,
+      resave: true,
+      saveUninitialized: false
+    })
+  );
+  
+  const { ExpressOIDC } = require('@okta/oidc-middleware');
+  console.log(`${process.env.OKTA_ORG_URL}/oauth2/default`);
+  const oidc = new ExpressOIDC({
+    
+    appBaseUrl: process.env.HOST_URL,
+    issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+    client_id: process.env.OKTA_CLIENT_ID,
+    client_secret: process.env.OKTA_CLIENT_SECRET,
+    redirect_uri: `${process.env.HOST_URL}/callback`,
+    scope: 'openid profile',
+    routes: {
+      loginCallback: {
+        path: '/callback'
+      },
+    }
+  });
+  
+  app.use(oidc.router);
 
 app.use('/', require('./routes/index'));
+app.use('/register', require('./routes/register'));
 
 const PORT = process.env.PORT || 3000;
 const localhost = "127.0.0.1";
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) =>{
-    res.render('index');
+    // To easily expose the routers I need to expose the user context to the view to 
+    // know if a suer is logged in or not so I know which buttons to display.
+    const {userContext } = req;
+    res.render('index', {userContext});
 });
+
+
 
 
 module.exports = router;
